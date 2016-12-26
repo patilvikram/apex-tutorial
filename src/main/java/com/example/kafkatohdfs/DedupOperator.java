2c2a5fa5304c842563dfdea158bf78ed59bd1df9
vikram <patilvikram@gmail.com>
2016-12-26 17:22:24 +0530

    Fixed code to update partition by specifing number of required partition through UI

--- src/main/java/com/example/kafkatohdfs/DedupOperator.java
+++ src/main/java/com/example/kafkatohdfs/DedupOperator.java
@@ -1,29 +1,28 @@
 package com.example.kafkatohdfs;
 
-import com.datatorrent.api.Context;
-import com.datatorrent.api.DefaultInputPort;
-import com.datatorrent.api.DefaultOutputPort;
+import com.datatorrent.api.*;
 import com.datatorrent.api.Operator.*;
-import com.datatorrent.api.StreamCodec;
 import com.datatorrent.common.util.BaseOperator;
 import com.datatorrent.lib.util.PojoUtils;
 import org.apache.apex.malhar.lib.dedup.DeduperStreamCodec;
+import org.apache.apex.shaded.ning19.com.ning.http.client.multipart.Part;
 
-import java.util.HashSet;
+import java.util.*;
 
 /**
  * Created by vikram on 22/12/16.
  */
-public class DedupOperator extends BaseOperator implements ActivationListener {
+public class DedupOperator extends BaseOperator implements ActivationListener,Partitioner<DedupOperator> ,StatsListener {
 
     public transient Class pojoClazz;
 
+
     public HashSet uniqueSet = new HashSet();
 
-    public PojoUtils.Getter getter = null;
+    public transient PojoUtils.Getter getter = null;
 
     private String keyExpr = null;
-    private StreamCodec inputCodec;
+    private transient  StreamCodec inputCodec;
 
 
     public final transient DefaultOutputPort<Object> unique = new DefaultOutputPort();
@@ -93,6 +92,75 @@ public class DedupOperator extends BaseOperator implements ActivationListener {
     }
 
 
+    public int getRequiredPartions() {
+        return requiredPartions;
+    }
+
+    public void setRequiredPartions(int requiredPartions) {
+        this.requiredPartions = requiredPartions;
+    }
+
+    public int getActualPartitions() {
+        return actualPartitions;
+    }
+
+    public void setActualPartitions(int actualPartitions) {
+        this.actualPartitions = actualPartitions;
+    }
+
+    int requiredPartions=1,actualPartitions=1;
 
 
+
+    public HashSet getUniqueSet() {
+        return uniqueSet;
+    }
+
+    public void setUniqueSet(HashSet uniqueSet) {
+        this.uniqueSet = uniqueSet;
+    }
+
+
+
+    @Override
+    public Collection<Partition<DedupOperator>> definePartitions(Collection<Partition<DedupOperator>> partitions, PartitioningContext context) {
+        if( partitions.size() == requiredPartions){
+            return partitions;
+        }
+
+        List<Partition<DedupOperator>> newPartitions = new ArrayList<Partition<DedupOperator>>(requiredPartions);
+        // Create combined states from all existing operators;
+
+        HashSet<Object> combinedSet = new HashSet<>();
+        for( Partition<DedupOperator> existingPartition: partitions){
+            DedupOperator dedup = existingPartition.getPartitionedInstance();
+            combinedSet.addAll(dedup.getUniqueSet());
+        }
+        for( int i=0;i<requiredPartions;i++){
+            DedupOperator dedup = new DedupOperator();
+            dedup.setKeyExpr(keyExpr);
+            dedup.setUniqueSet(combinedSet);
+            Partition<DedupOperator> part = new DefaultPartition<DedupOperator>(dedup);
+            newPartitions.add(part);
+        }
+
+        DefaultPartition.assignPartitionKeys(newPartitions, input);
+        return newPartitions;
+    }
+
+    @Override
+    public void partitioned(Map<Integer, Partition<DedupOperator>> partitions) {
+        actualPartitions = partitions.size();
+    }
+
+    @Override
+    public Response processStats(BatchedOperatorStats stats) {
+        if (getRequiredPartions() != actualPartitions) {
+            Response resp = new Response();
+            resp.repartitionRequired = true;
+            return resp;
+        }
+        return null;
+
+    }
 }
--- src/main/java/com/example/kafkatohdfs/Kafka2HDFSWithDedupApplication.java
+++ src/main/java/com/example/kafkatohdfs/Kafka2HDFSWithDedupApplication.java
@@ -1,21 +1,25 @@
 package com.example.kafkatohdfs;
 
-import com.datatorrent.api.DAG;
-import com.datatorrent.api.StreamingApplication;
+import com.datatorrent.api.*;
 import com.datatorrent.api.annotation.ApplicationAnnotation;
 import com.datatorrent.contrib.formatter.CsvFormatter;
 import com.datatorrent.contrib.parser.CsvParser;
 import com.datatorrent.lib.filter.FilterOperator;
 import com.datatorrent.lib.io.ConsoleOutputOperator;
+import com.datatorrent.lib.partitioner.StatelessThroughputBasedPartitioner;
 import org.apache.apex.malhar.kafka.KafkaSinglePortInputOperator;
 import org.apache.apex.malhar.lib.fs.GenericFileOutputOperator;
 import org.apache.hadoop.conf.Configuration;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Map;
 
 /**
  * Created by vikram on 23/12/16.
  */
 @ApplicationAnnotation(name = "Kafka2HDFSWithDedup")
-public class Kafka2HDFSWithDedupApplication implements StreamingApplication {
+public class Kafka2HDFSWithDedupApplication implements StreamingApplication{
     @Override
     public void populateDAG(DAG dag, Configuration conf) {
             /*
@@ -38,8 +42,13 @@ public class Kafka2HDFSWithDedupApplication implements StreamingApplication {
         dag.addStream("dedupOutput",dedupOperator.unique,csvFormatter.in);
 //        dag.addStream("dataOut", csvFormatter.out, outputOperator.input).setLocality(DAG.Locality.THREAD_LOCAL);
         dag.addStream(  "dataConsoleOut",csvFormatter.out,consoleOutputOperator.input);
+        StatelessThroughputBasedPartitioner partitioner = new StatelessThroughputBasedPartitioner();
+        partitioner.setInitialPartitionCount(1);
+        partitioner.setMinimumEvents(10);
+        partitioner.setMaximumEvents(100);
+        partitioner.setCooldownMillis(30000);
 
+        dag.setAttribute(dedupOperator, Context.OperatorContext.STATS_LISTENERS, Arrays.asList(new StatsListener[]{partitioner}));
 
     }
-
 }
